daa66f04289085b5d544cb3fefb18758a8c2a44f
dorri-riddo <cider134@gmail.com>
2020-02-06 10:26:57 +0900

    백준 알고리즘

---
+++ b_3052/Main.java
@@ -0,0 +1,32 @@
+package b_3052;
+
+import java.util.Scanner;
+
+public class Main {
+	//https://www.acmicpc.net/problem/3052
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] array = new int[10];
+		int[] remainder = new int[10];
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = sc.nextInt();
+			remainder[i] = array[i] % 42;
+		}
+		
+		int count = 0;
+		
+		for (int i = 0; i < remainder.length; i++) {
+			if (remainder[i] == -1)
+				continue;
+			for (int j = i + 1; j < remainder.length; j++) {
+				if (remainder[i] == remainder[j])
+					remainder[j] = -1;
+			}
+			count++;
+		}
+		
+		System.out.print(count);
+	}
+}
