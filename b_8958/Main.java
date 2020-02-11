6b7566dd74cc7b6768383be21994201bd005157f
dorri-riddo <cider134@gmail.com>
2020-02-11 10:17:03 +0900

    백준 알고리즘

---
+++ b_8958/Main.java
@@ -0,0 +1,27 @@
+package b_8958;
+
+import java.util.Scanner;
+
+public class Main {
+	//https://www.acmicpc.net/problem/8958
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		while(T-- > 0) {
+			String quiz = sc.next();
+			
+			int count = 0;
+			int score = 0;
+			for (int i = 0; i < quiz.length(); i++) {
+				if (quiz.substring(i, i+1).equals("O")) 
+					score += ++count;
+				else 
+					count = 0;
+			}
+			
+			System.out.println(score);
+		}
+		
+	}
+}
