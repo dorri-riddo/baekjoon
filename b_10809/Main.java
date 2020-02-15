4a99de721cb0dfb0ecf183942c94c74a984e0bf2
dorri-riddo <cider134@gmail.com>
2020-02-15 15:37:21 +0900

    백준 알고리즘

---
+++ b_10809/Main.java
@@ -0,0 +1,28 @@
+package b_10809;
+
+import java.util.Scanner;
+
+public class Main {
+	//https://www.acmicpc.net/problem/10809
+	public static void main(String[] args) {	
+		Scanner sc = new Scanner(System.in);
+		
+		String S = sc.next();
+		String[] array = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		for (int i = 0; i < S.length(); i++) {
+			for (int j = 0; j < array.length; j++) {
+				if (S.substring(i, i + 1).equals(array[j]))
+					array[j] = "" + i;
+			}
+		}
+	
+		for (int i = 0; i < array.length; i++) {
+			if (97 <= (int) array[i].charAt(0) && 122 >= (int) array[i].charAt(0))
+				array[i] = "-1";
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+}
---
+++ b_11720/Main.java
@@ -0,0 +1,21 @@
+package b_11720;
+
+import java.util.Scanner;
+
+public class Main {
+	//https://www.acmicpc.net/problem/11720
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		String str = sc.next();
+		int sum = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			int temp = Integer.parseInt(str.substring(i, i + 1));
+			sum += temp;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
