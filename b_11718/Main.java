a4b0724d9b7345b8eeed8115364b165c0529c57b
dorri-riddo <cider134@gmail.com>
2020-02-25 20:33:48 +0900

    그대로 출력하기

---
+++ b_11718/Main.java
@@ -0,0 +1,13 @@
+package b_11718;
+
+import java.util.Scanner;
+
+public class Main {
+	//https://www.acmicpc.net/problem/11718
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext())
+			System.out.println(sc.nextLine());
+	}
+
+}
