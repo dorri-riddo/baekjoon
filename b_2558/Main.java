ef97a18c386166e92d661b1dc94b18d3d46085fa
dorri-riddo <cider134@gmail.com>
2020-02-25 20:33:36 +0900

    A+B - 2

---
+++ b_2558/Main.java
@@ -0,0 +1,15 @@
+package b_2558;
+
+import java.util.Scanner;
+
+public class Main {
+	//https://www.acmicpc.net/problem/2558
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.print(A + B);
+	}
+
+}
