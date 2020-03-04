9b98d1dabfbc42a5780b0b309c9aa498cbd38944
dorri-riddo <cider134@gmail.com>
2020-03-04 12:00:22 +0900

    생일 출력하기

---
+++ b_2555/Main.java
@@ -0,0 +1,20 @@
+package b_2555;
+
+import java.util.Scanner;
+
+public class Main {
+	//https://www.acmicpc.net/problem/2555
+	public static void main(String[] args) {
+		// 입력 X 만든 사람의 생일..
+//		Scanner sc = new Scanner(System.in);
+//		int month = sc.nextInt();
+//		int day = sc.nextInt();
+//		sc.close();
+//		
+//		System.out.print(String.format("%d/%d", month, day));
+		
+		System.out.print(String.format("%d/%d", 10, 14));
+
+	}
+
+}
