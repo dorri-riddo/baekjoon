619456bd706b5991f77686088a3ed7686bf6fbca
dorri-riddo <cider134@gmail.com>
2020-03-04 12:00:09 +0900

    웰컴

---
+++ b_5337/Main.java
@@ -0,0 +1,14 @@
+package b_5337;
+
+public class Main {
+	//https://www.acmicpc.net/problem/5337
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(".  .   .\n" + 
+				"|  | _ | _. _ ._ _  _\n" + 
+				"|/\\|(/.|(_.(_)[ | )(/.");
+		System.out.print(sb.toString());
+
+	}
+
+}
