4a3e575eb79a99a9ce27a9c86de6a34bd1195f00
dorri-riddo <cider134@gmail.com>
2020-03-04 12:00:37 +0900

    마이크로소프트 로고

---
+++ b_5338/Main.java
@@ -0,0 +1,16 @@
+package b_5338;
+
+public class Main {
+	//https://www.acmicpc.net/problem/5338
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("       _.-;;-._\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-;;-._|\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-''-._|");
+		System.out.print(sb.toString());
+
+	}
+
+}
