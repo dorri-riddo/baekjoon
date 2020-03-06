8da2cc513fbcbebb8e9ba42f541b204253ca18cd
dorri-riddo <cider134@gmail.com>
2020-03-06 11:43:53 +0900

    홍익대학교

---
+++ b_16394/Main.java
@@ -0,0 +1,12 @@
+package b_16394;
+
+import java.util.Scanner;
+
+public class Main {
+	//https://www.acmicpc.net/problem/16394
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(sc.nextInt() - 1946);
+		sc.close();
+	}
+}
