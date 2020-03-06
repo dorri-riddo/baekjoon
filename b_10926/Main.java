aa85a18d3883aabbee170adbfe7bcaf0992e74b0
dorri-riddo <cider134@gmail.com>
2020-03-06 11:42:49 +0900

    ??!

---
+++ b_10926/Main.java
@@ -0,0 +1,16 @@
+package b_10926;
+
+import java.util.Scanner;
+
+public class Main {
+	//https://www.acmicpc.net/problem/10926
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		sc.close();
+		
+		System.out.print(str + "??!");
+
+	}
+
+}
