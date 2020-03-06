6b1b6d190c3f58801ef4d40724c50a23c3ef2948
dorri-riddo <cider134@gmail.com>
2020-03-06 11:44:08 +0900

    단어 길이 재기

---
+++ b_2743/Main.java
@@ -0,0 +1,13 @@
+package b_2743;
+
+import java.util.Scanner;
+
+public class Main {
+	// https://www.acmicpc.net/problem/2743
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(sc.nextLine().length());
+		sc.close();
+	}
+
+}
