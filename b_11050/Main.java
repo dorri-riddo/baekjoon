3dec442b049f4bd24cb05d7136cbf0b77746f16e
dorri-riddo <cider134@gmail.com>
2020-03-08 22:20:52 +0900

    이항 계수 1

---
+++ b_11050/Main.java
@@ -0,0 +1,26 @@
+package b_11050;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		sc.close();
+		
+		int result = 1;
+		
+		for (int i = n; i >= n - k + 1; i--) {
+			result *= i;
+		}
+		for (int i = 1; i <= k; i++) {
+			result /= i;
+		}
+		
+		System.out.print(result);
+
+	}
+
+}
