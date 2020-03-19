4674c01a800fd49230cf0ff478f5ca0cf0ee7001
dorri-riddo <cider134@gmail.com>
2020-03-19 11:06:43 +0900

    사분면 고르기

---
+++ b_14681/Main.java
@@ -0,0 +1,23 @@
+package b_14681;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		sc.close();
+		
+		if (x > 0 && y > 0) {
+			System.out.print(1);
+		} else if (x > 0 && y < 0) {
+			System.out.print(4);
+		} else if (x < 0 && y < 0) {
+			System.out.print(3);
+		} else {
+			System.out.print(2);
+		}
+	}
+}
