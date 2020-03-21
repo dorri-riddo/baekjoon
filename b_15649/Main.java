5638a2cf92335378fce85d4cec8e50eff8cf7abc
dorri-riddo <cider134@gmail.com>
2020-03-21 10:28:03 +0900

    Revert "Revert "사파리월드""
    This reverts commit 3808d973c85c360777d2efd62e0aaaf11176242d.

---
+++ b_15649/Main.java
@@ -0,0 +1,22 @@
+package b_15649;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		sc.close();
+		
+		int[][] array = new int[n][m];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+			}
+		}
+		
+	}
+}
