84d07376b781f866a6eea8c1d131a34c2fc09063
dorri-riddo <cider134@gmail.com>
2020-03-22 13:54:08 +0900

    N과 M (1)

--- b_15649/Main.java
+++ b_15649/Main.java
@@ -6,17 +6,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
 		int n = sc.nextInt();
 		int m = sc.nextInt();
+		int[] array = new int[m];
+		boolean[] visited = new boolean[n + 1];
 		sc.close();
 		
-		int[][] array = new int[n][m];
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[0].length; j++) {
+		func(n, m, array, visited, 0);
+	}
+
+	// 백 트래킹 알고리즘
+	public static void func(int n, int m, int[] array, boolean[] visited, int pos) {
+		if (pos == m) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < array.length; i++) {
+				sb.append(array[i] + " ");
 			}
+			System.out.print(sb.toString() + "\n");
+			return;
 		}
 		
+		for (int i = 1; i <= n; i++) {
+			if (!visited[i]) {
+				array[pos] = i;
+				visited[i] = true;
+				func(n, m, array, visited, pos + 1);
+				visited[i] = false;
+			}
+		}
 	}
 }
