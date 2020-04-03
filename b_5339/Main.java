16690bb61a78fa60a411af5c67dab9f2e5a696d7
dorri-riddo <cider134@gmail.com>
2020-04-03 11:54:33 +0900

    콜센터

---
+++ b_5339/Main.java
@@ -0,0 +1,21 @@
+package b_5339;
+
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("     /~\\\n" + 
+				"    ( oo|\n" + 
+				"    _\\=/_\n" + 
+				"   /  _  \\\n" + 
+				"  //|/.\\|\\\\\n" + 
+				" ||  \\ /  ||\n" + 
+				"============\n" + 
+				"|          |\n" + 
+				"|          |\n" + 
+				"|          |");
+		System.out.print(sb.toString());
+		
+	}
+
+}
