45b61a2c9df28717952ea95b41189e0c1acaa38c
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:31:20 +0900

    N 찍기

--- b_2741/Main.java
+++ b_2741/Main.java
@@ -3,7 +3,6 @@ package b_2741;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/2741
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
