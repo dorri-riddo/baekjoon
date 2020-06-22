8347e211d477000aa4ee39476a3c8cb7ed0867e0
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 15:05:17 +0900

    한수

--- b_1065/Main.java
+++ b_1065/Main.java
@@ -3,7 +3,6 @@ package b_1065;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/1065
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
