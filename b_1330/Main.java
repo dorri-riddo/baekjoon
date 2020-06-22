34cb5dfd26d563bb116716b76bc3fd9f2bcedc19
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:29:18 +0900

    두 수 비교하기

--- b_1330/Main.java
+++ b_1330/Main.java
@@ -3,7 +3,6 @@ package b_1330;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/1330
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
