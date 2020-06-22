e3d80b06b8605ce7948b19cb8f37c72954b4985b
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:30:08 +0900

    별 찍기 - 2

--- b_2439/Main.java
+++ b_2439/Main.java
@@ -3,7 +3,6 @@ package b_2439;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
