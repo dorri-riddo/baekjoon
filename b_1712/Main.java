aa2b86569eaa785ad21f689e657cf688147f5b85
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:29:46 +0900

    손익분기점

--- b_1712/Main.java
+++ b_1712/Main.java
@@ -3,7 +3,6 @@ package b_1712;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/1712
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int A = sc.nextInt();
