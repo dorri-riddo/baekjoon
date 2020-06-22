77c00eaa5d09741bc0913b307d8b9f45806d8a0b
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:27:25 +0900

    단어의 개수

--- b_1152/Main.java
+++ b_1152/Main.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		String str = bf.readLine();
