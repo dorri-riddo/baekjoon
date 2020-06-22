c00f88a3584d28b244432e4815d289bcea7f6975
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:27:39 +0900

    아스키 코드

--- b_11654/Main.java
+++ b_11654/Main.java
@@ -3,7 +3,6 @@ package b_11654;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/11654
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
