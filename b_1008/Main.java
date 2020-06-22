6eaf1128ea4c46897b8b59f14eeb051ce872b283
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 14:53:59 +0900

    A/B

--- b_1008/Main.java
+++ b_1008/Main.java
@@ -3,7 +3,6 @@ package b_1008;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/1008
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int A = sc.nextInt();
