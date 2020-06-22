01aba2e584d1944f38a41fe4e3e0af861a3a3a2d
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 15:24:08 +0900

    사칙연산

--- b_10869/Main.java
+++ b_10869/Main.java
@@ -3,7 +3,6 @@ package b_10869;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/10869
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int A = sc.nextInt();
