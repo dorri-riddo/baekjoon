55a248ef68ad273554c87e7db89ff54eba9f729a
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:43:31 +0900

    설탕 배달

--- b_2839/Main.java
+++ b_2839/Main.java
@@ -3,7 +3,6 @@ package b_2839;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/2839
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
