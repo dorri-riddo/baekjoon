1373dd9089957b0b310cb59de279c819692d6af0
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:27:57 +0900

    두 수 비교하기

--- b_1316/Main.java
+++ b_1316/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/1316
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
