7fe7b6d8a528e554daf4339a7b1f98f61a1debb0
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:44:32 +0900

    다이얼

--- b_5622/Main.java
+++ b_5622/Main.java
@@ -3,7 +3,6 @@ package b_5622;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/5622
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
