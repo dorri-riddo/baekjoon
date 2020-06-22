41a047d6664f7c700f07da42d150d026b7027849
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 15:24:30 +0900

    A + B - 3

--- b_10950/Main.java
+++ b_10950/Main.java
@@ -3,7 +3,6 @@ package b_10950;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/10950
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
