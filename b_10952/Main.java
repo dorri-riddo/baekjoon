79aecdc4d9f955f63c113b1cc598a10b7dc5b234
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:26:33 +0900

    A+B - 5

--- b_10952/Main.java
+++ b_10952/Main.java
@@ -3,7 +3,6 @@ package b_10952;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/10952
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
