eaa8bf36c7c16c3f4ecfb36e8d4a8a4313e7d59b
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:31:40 +0900

    윤년

--- b_2753/Main.java
+++ b_2753/Main.java
@@ -3,7 +3,6 @@ package b_2753;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/2753
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int year = sc.nextInt();
