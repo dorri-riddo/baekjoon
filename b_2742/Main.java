d52e68e957b165119b17277e4247f3f71505193e
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:31:29 +0900

    기찍 N

--- b_2742/Main.java
+++ b_2742/Main.java
@@ -3,7 +3,6 @@ package b_2742;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/2742
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
