1da1117cec625fc5de3c729ab24653906bcec304
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:44:38 +0900

    합

--- b_8393/Main.java
+++ b_8393/Main.java
@@ -3,7 +3,6 @@ package b_8393;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/8393
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
