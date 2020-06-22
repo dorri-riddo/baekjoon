043a6c15a6774680b8524e3b8077d44e658b00f0
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:44:47 +0900

    OX퀴즈

--- b_8958/Main.java
+++ b_8958/Main.java
@@ -3,7 +3,6 @@ package b_8958;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/8958
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
