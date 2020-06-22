d08018bbcccf25b83775d71a63a7eff788accbdb
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 16:27:19 +0900

    더하기 사이클

--- b_1110/Main.java
+++ b_1110/Main.java
@@ -3,7 +3,6 @@ package b_1110;
 import java.util.Scanner;
 
 public class Main {
-	//https://www.acmicpc.net/problem/1110
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
