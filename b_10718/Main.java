2c47c08cc86747810d0909de82d0d3e647fe0dd3
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-22 15:05:27 +0900

    We love kriii

--- b_10718/Main.java
+++ b_10718/Main.java
@@ -1,7 +1,6 @@
 package b_10718;
 
 public class Main {
-	//https://www.acmicpc.net/problem/10718
 	public static void main(String[] args) {
 		for (int i = 0; i < 2; i++) {
 			System.out.println("강한친구 대한육군");
