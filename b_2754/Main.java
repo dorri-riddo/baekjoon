4ca9b4a970f637f23db960cba6d5e7dadade17f0
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-06-29 17:20:18 +0900

    학점계산

---
+++ b_2754/Main.java
@@ -0,0 +1,57 @@
+package b_2754;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		sc.close();
+		String result = "";
+		
+		switch(str) {
+		case "A+" :
+			result = "4.3";
+			break;
+		case "A0" :
+			result = "4.0";
+			break;
+		case "A-" :
+			result = "3.7";
+			break;
+		case "B+" :
+			result = "3.3";
+			break;
+		case "B0" :
+			result = "3.0";
+			break;
+		case "B-" :
+			result = "2.7";
+			break;
+		case "C+" :
+			result = "2.3";
+			break;
+		case "C0" :
+			result = "2.0";
+			break;
+		case "C-" :
+			result = "1.7";
+			break;
+		case "D+" :
+			result = "1.3";
+			break;
+		case "D0" :
+			result = "1.0";
+			break;
+		case "D-" :
+			result = "0.7";
+			break;
+		case "F" : 
+			result = "0.0";
+			break;
+		}
+		System.out.println(result);
+	}
+
+}
