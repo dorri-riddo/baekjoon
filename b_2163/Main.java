8e3169c7c7da0c5defeda049c901ba61ee36f490
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-07-13 15:31:44 +0900

    초콜릿 자르기

---
+++ b_2163/Main.java
@@ -0,0 +1,17 @@
+package b_2163;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		sc.close();
+		
+		System.out.println(n * m - 1);
+
+	}
+
+}
